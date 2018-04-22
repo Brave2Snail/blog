d8d373a5bd610a395ec5973a2804e3fa71a4707a
SnailPy <940614896@qq.com>
2018-04-22 09:14:00 +0800

    class one is done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class BlogArticles(models.Model):
+	title = models.CharField(max_length=100)
+	author = models.ForeignKey(User, related_name='blog_posts')
+	body = models.TextField()
+	publish = models.DateTimeField(default=timezone.now)
+
+	class Meta:
+		ordering = ('-publish',)
+
+	def __str__(self):
+		return self.title
+
+
